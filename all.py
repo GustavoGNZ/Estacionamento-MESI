ef6eccf1c9e497a0d92cba3f83f40c51789a72d0
GustavoGNZ <gustavo.ganaza@hotmail.com>
2024-07-24 17:23:56 -0300

    add writeBack and Fifo

--- all.py
+++ all.py
@@ -35,16 +35,21 @@ class CacheLine:
         self.data = data
         self.state = state
 
+    def printLine(self):
+        print(f"Endereço = {self.endereco}, Dado = {self.data}, Estado = {self.state}")
+
 class Cache:
     def __init__(self, size):
         self.lines = [CacheLine() for _ in range(size)]
         self.size = size
+        self.fifoQueue = []
 
     def search(self, endereco):
         for line in self.lines:
             if line.endereco == endereco:
                 return line
         return None
+
 
     def write(self, endereco, data, state):
         line = self.search(endereco)
@@ -53,10 +58,25 @@ class Cache:
         else:
             line = self.search(None)
             if line:
-                line.update(endereco, data, state)
-            else:
-                line = random.choice(self.lines)
-                line.update(endereco, data, state)
+                 # Filtra apenas as linhas vazias
+                empty_lines = [l for l in self.lines if l.endereco is None]
+                if empty_lines:
+                    # Escolhe uma linha vazia aleatoriamente
+                    line = random.choice(empty_lines)
+                    line.update(endereco, data, state)
+                    self.fifoQueue.append(self.lines.index(line))
+            elif self.is_full():
+                # fifo replacement
+                # fifo replacement
+                index = self.fifoQueue.pop(0)
+                line_to_remove = self.lines[index]
+                endereco_to_remove = line_to_remove.endereco
+                data_to_remove = line_to_remove.data
+                self.lines[index] = CacheLine()
+                self.write(endereco, data, state)
+
+                return endereco_to_remove, data_to_remove
+        return None
 
     def read(self, endereco, memory, cache_manager, processor_id):
         line = self.search(endereco)
@@ -82,6 +102,10 @@ class Cache:
             estado = line.state.value if line.state else "N/A"
             print(f"Linha {i}: Endereço = {line.endereco}, Dado = {line.data}, Estado = {estado}")
         print()
+
+    def is_full(self):
+        return len(self.lines) >= self.size
+            
 
 class CacheManager:
     def __init__(self):
@@ -114,21 +138,25 @@ class CacheManager:
                     line.state = State.SHARED
 
     def handle_read(self, processor_id, endereco, memory):
-        cache = self.get_cache(processor_id)
-        line = cache.search(endereco)
-        if line and line.state != State.INVALID:
-            if line.state == State.EXCLUSIVE:
-                self.update_state_to_shared(endereco, processor_id)
-            return line.data
-        else:
-            data = cache.read(endereco, memory, self, processor_id)
-            return data
+
+        for pid, cache in self.caches.items():
+            line = cache.search(endereco)
+            if line and line.state != State.INVALID:
+                if line.state == State.EXCLUSIVE:
+                    self.update_state_to_shared(endereco, processor_id)
+                    self.caches[processor_id].write(endereco, line.data, State.SHARED)
+                return line.data
+            else:
+                data = cache.read(endereco, memory, self, processor_id)
+                return data
 
     def handle_write(self, processor_id, endereco, data, memory):
         self.invalidate_other_caches(endereco, processor_id)
         cache = self.get_cache(processor_id)
-        cache.write(endereco, data, State.MODIFIED)
-        memory.write(endereco, data)
+        result = cache.write(endereco, data, State.MODIFIED)
+        if result != None:
+            endereco_to_add, data_to_add = result
+            memory.write(endereco_to_add, data_to_add)
 
 class Processador:
     def __init__(self, id, cache_size, memory, cache_manager):
@@ -145,41 +173,39 @@ class Processador:
 
     def escrever(self, endereco, valor):
         self.cache_manager.handle_write(self.id, endereco, valor, self.memory)
-        print(f"Processador {self.id} escreve o valor {valor} no endereço {endereco}")
+        print(f"Processador {self.id} escreve o valor {valor} de endereço {endereco}")
 
     def print_cache(self):
         self.cache.print_cache(self.id)
 
-# def main():
-#     memory = Memory(5)
-#     cache_manager = CacheManager()
+def main():
+    memory = Memory(10)
+    cache_manager = CacheManager()
 
-#     # Inicializa os processadores
-#     processor1 = Processador(1, 4, memory, cache_manager)
-#     processor2 = Processador(2, 4, memory, cache_manager)
-#     processor3 = Processador(3, 4, memory, cache_manager)
+    # Inicializa os processadores
+    processor1 = Processador(1, 4, memory, cache_manager)
+    processor2 = Processador(2, 4, memory, cache_manager)
+    processor3 = Processador(3, 4, memory, cache_manager)
 
-#     # Realiza algumas operações de leitura e escrita
-#     print("Criando Estado Exclusivo (E):")
-#     processor1.ler(3)  # Isso deve colocar a linha na cache de processor1 como EXCLUSIVE
-#     processor1.print_cache()
+    memory.print_memory()
+    processor1.print_cache()
 
-#     print("Processador 2 lê o endereço 30 (Estado deve mudar para SHARED):")
-#     processor2.ler(3)  # Isso deve colocar a linha na cache de processor2 como SHARED
-#     processor1.print_cache()
-#     processor2.print_cache()
+    processor1.escrever(0, 1111)
+    processor1.escrever(1, 2222)
+    processor1.escrever(3, 4444)
+    processor1.escrever(2, 3333)
 
-#     print("Processador 3 escreve no endereço 30 (Invalida as caches de processor1 e processor2):")
-#     processor3.escrever(3, 8888)  # Isso deve invalidar as linhas de cache de processor1 e processor2
-#     processor1.print_cache()
-#     processor2.print_cache()
-#     processor3.print_cache()
+    processor1.print_cache()
+    # processor1.escrever(3, 4444)
+    processor1.escrever(6, 5555) 
 
-#     # Imprime a memória principal e o estado das caches
-#     memory.print_memory()
-#     processor1.print_cache()
-#     processor2.print_cache()
-#     processor3.print_cache()
+    processor1.print_cache()
 
-# if __name__ == "__main__":
-#     main()
+    processor1.escrever(7, 6666)   
+
+    # Imprime a memória principal e o estado das caches
+    memory.print_memory()
+    processor1.print_cache()
+
+if __name__ == "__main__":
+    main()
